1f4e44446ffb8949290431a49b8d012aaf3d1d32
CipherKT <ktcipher@gmail.com>
2024-04-17 23:42:31 +0530

    frontend issue

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileUploadForm from './upload-page.js';
+
+
+function App() {
+    return (
+        <div>
+            <FileUploadForm />
+           
+        </div>
+    );
+}
+
+export default App;
